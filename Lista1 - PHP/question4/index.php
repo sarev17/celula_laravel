56d3ad5ccdcd2d297f919edf3cd211cd642f252b
Alan <alanreislm@gmail.com>
2023-03-13 13:53:13 -0300

    add question4

---
+++ Lista1 - PHP/question4/index.php
@@ -0,0 +1,6 @@
+<form method="post" action="page2.php">
+    <?php
+        echo "Digite um número: <input type='text' name='input_$n'>";
+    ?>
+    <input type="submit" value="ENVIAR">
+</form>
---
+++ Lista1 - PHP/question4/page2.php
@@ -0,0 +1,9 @@
+<?php
+    $number = $_POST["input_$n"];
+
+    for ($i = 1; $i <= 10; $i++) { 
+        echo $i . " X " . $number . " = " . ($i*$number) . "<br/>";
+    }
+?>
+
+
