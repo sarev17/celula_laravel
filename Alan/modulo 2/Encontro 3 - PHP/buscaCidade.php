3a49b80d729fa1fa4899933a2d5a12567db4480c
Alan <alanreislm@gmail.com>
2023-04-07 11:48:45 -0300

    add: example of date 3

--- Alan/modulo 2/Encontro 3 - PHP/buscaCidade.php
+++ Alan/modulo 2/Encontro 3 - PHP/buscaCidade.php
@@ -1,5 +1,16 @@
 <?php
 
+    $url = "cidades.json";
+    $json = file_get_contents($url);
+    $array = json_decode($json, true);
+    $busca = $_GET["cidade"];
 
+    $cidades = array_filter($array, function($cidade)use($busca){
+        return stripos($cidade["nome"], $busca) !== false;
+    });
 
+    $cidades = array_values($cidades);
+    $cidades = json_encode($cidades);
+
+    echo $cidades;
 ?>
