be9f1434619274554c27d82e50748a9ee4edf393
Alan <alanreislm@gmail.com>
2023-04-07 16:46:17 -0300

    add: atv meeting 4

--- Alan/modulo 2/Encontro 4 - PHP/processamento.php
+++ Alan/modulo 2/Encontro 4 - PHP/processamento.php
@@ -1,5 +1,35 @@
 <?php
+    include "calculoOperacoes.php";
+    include "validador.php";
+
+    $valor1 = $_POST['valor1'];
+    $valor2 = $_POST['valor2'];
+    $operacao = strval($_POST['operacao']);
 
 
+    if(!(verificarCamposVazios($_POST))){
+        header("Location: index.php?erro=Todos os campos são obrigatórios");
+        exit;
+    }else{
+        if(!verificarNumeros($valor1, $valor2)){
+            header("Location: index.php?erro=Insira somente números");
+            exit;
+        }else{
+            if($operacao == "A"){
+                header("Location: index.php?result=".soma($valor1, $valor2));
+            }elseif($operacao == "S"){
+                header("Location: index.php?result=".subtracao($valor1, $valor2));
+            }elseif($operacao == "M"){
+                header("Location: index.php?result=".multiplicacao($valor1, $valor2));
+            }elseif($operacao == "D") {
+                if(verificarIndeterminação($valor2)){
+                    header("Location: index.php?erro=Coloque um número diferente de ZERO no VALOR 2");
+                    exit;
+                }else{
+                    header("Location: index.php?result=".divisao($valor1, $valor2));
+                }
+            }
+        }
+    }
 
 ?>
