a9336eb12cc500c0520df8851b4bc3a2dbbe9154
sarev17 <sarev17@gmail.com>
2023-04-16 20:32:10 -0300

    fix(folder_php): Atualoizando pasta php

--- material dos encontros/PHP/teste/routes/create_product.php
+++
@@ -1,8 +0,0 @@
-<?php
-    include '../controllers/ProductController.php';
-
-    $data = file_get_contents('php://input');
-    $data = json_decode($data,true);
-    $product = new ProductController($data);
-    echo $product->create();
-?>
