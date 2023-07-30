ddfdc7d4293d66b308bd5a5b373e31de158d0a62
Paulo Ricardo <125914780+prsousa8@users.noreply.github.com>
2023-07-30 12:54:42 -0300

    Update index.php

--- Paulo Ricardo/modulo 2/E5/index.php
+++ Paulo Ricardo/modulo 2/E5/index.php
@@ -1,1 +1,18 @@
-
+<?php
+    include 'conecta.php';
+    $date=date('d/m/Y');
+    $data = [
+        'nome'=>'Maria',
+        'email'=>'oi@gmail.com',
+        'telefone'=>'12 9 1234-5678',
+        'data'=>$date
+    ];
+    $nome='Maria';
+    $email='hello$outlook.com';
+    //criar_tabela();
+    //insere_cliente($data);
+    //listar_registros();
+    //buscar_registro('Paulo');
+    //atualizar($nome,$email);
+    //excluir_registro($nome);
+?>
